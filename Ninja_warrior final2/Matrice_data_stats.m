a2b17d79c96d2b20bb67dc805731669080a40ea7
maximehuotlavoie <maxime.huot-lavoie.1@ulaval.ca>
2018-10-25 16:52:30 -0400

    correction répétition

--- Ninja_warrior final2/Matrice_data_stats.m
+++ Ninja_warrior final2/Matrice_data_stats.m
@@ -3,28 +3,10 @@
  
  
  %% Enregistrement nouvelles donnees dans matrice
- i= % Cellule rats dans Resume_data
- temps=Resume_data{1,i}.t_passage_compil'; 
- jours=4; % Numéro de l'expérience en cours /8
- rats=9; % No du rats A=1 B=2
- matrice_data(1:numel(temps),jours,rats)=temps;
- 
-
- 
- %% Filtrer la matrice en fonction du temps de parcours maximum déterminé
- position=find(matrice_data>0.1);
- 
- matrice_data_filtered=matrice_data;
- matrice_data_filtered(position)=NaN;
- 
- %% 
-matrice_data=NaN(100,30,20);
-
-%%
-i=3;
+ i=3;% Cellule rats dans Resume_data
 temps=Resume_data{1,i}.t_passage_compil';
-jours=5;
-rat=3;
+jours=5;% Numéro de l'expérience en cours /8
+rat=3;% No du rats A=1 B=2
 
 if numel(temps)<101;
     matrice_data(1:numel(temps),jours,rat)=temps;
@@ -33,9 +15,16 @@ else
     matrice_data(1:100,jours,rat)=temps(1:100);
 end
 
+ 
+ %% Filtrer la matrice en fonction du temps de parcours maximum déterminé
+ position=find(matrice_data>0.1);
+ 
+ matrice_data_filtered=matrice_data;
+ matrice_data_filtered(position)=NaN;
+ 
 %% Anova
 
-y=matrice_data(1:40,1:5,2)
+y=matrice_data(1:40,1:5,2) %%% pourait ajouter matrice_data_filtered à la place de matrice_data
 group={'j1','j2','j3','j4','j5'};
 %%
 [p,tbl,stats]=anova1(y,group);
